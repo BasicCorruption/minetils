9cd743868eb5b3f619b91e8c4ba4ce018cf4a862
Sid <68089579+BasicCorruption@users.noreply.github.com>
2022-05-03 18:29:09 -0400

    add checks for config

--- minetils/src/net/ddns/mlgland/minetils/listeners/player/OnPlayerJoin.java
+++ minetils/src/net/ddns/mlgland/minetils/listeners/player/OnPlayerJoin.java
@@ -21,7 +21,7 @@ public class OnPlayerJoin {
 
             player.kickPlayer("§4You have been banned indefinitely: " + reason);
         } else {
-            if (!cf.get("joinMessage").equals("")) {
+            if (!cf.get("enableJoinMessage").equals(true)) {
                 player.sendMessage(cf.get("joinMessage").toString());
             }
         }
