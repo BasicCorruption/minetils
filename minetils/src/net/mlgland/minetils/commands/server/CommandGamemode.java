d032c47462edd4f5d2c0278480fd6b63980ca2cc
BasicCorruption <saytleader@outlook.com>
2022-06-21 17:35:31 -0400

    Update CommandGamemode.java

--- minetils/src/net/mlgland/minetils/commands/server/CommandGamemode.java
+++ minetils/src/net/mlgland/minetils/commands/server/CommandGamemode.java
@@ -16,52 +16,52 @@ public class CommandGamemode {
             switch (command.toString()) {
                 case "gms":
                 case "gm0":
-                    case "gmsurvival":
-                        case "gsurvival":
-                            case "gmodesurvival":
-                                case "gamemodesurvival":
-                                    case "gamemode0":
-                                        case "gmode0":
-                                            case "gmod0":
-                                                // switch the gamemode to survival
-                                                player.setGameMode(SURVIVAL);
-                                                break;
+                case "gmsurvival":
+                case "gsurvival":
+                case "gmodesurvival":
+                case "gamemodesurvival":
+                case "gamemode0":
+                case "gmode0":
+                case "gmod0":
+                    // switch the gamemode to survival
+                    player.setGameMode(SURVIVAL);
+                    break;
                 case "gmc":
-                    case "gm1":
-                        case "gmcreative":
-                            case "gcreative":
-                                case "gmodecreative":
-                                    case "gamemodecreative":
-                                        case "gamemode1":
-                                            case "gmode1":
-                                                case "gmod1":
-                                                    // switch the gamemode to creative
-                                                    player.setGameMode(CREATIVE);
-                                                    break;
+                case "gm1":
+                case "gmcreative":
+                case "gcreative":
+                case "gmodecreative":
+                case "gamemodecreative":
+                case "gamemode1":
+                case "gmode1":
+                case "gmod1":
+                    // switch the gamemode to creative
+                    player.setGameMode(CREATIVE);
+                    break;
                 case "gma":
-                    case "gm2":
-                        case "gmadventure":
-                            case "gadventure":
-                                case "gmodeadventure":
-                                    case "gamemodeadventure":
-                                        case "gamemode2":
-                                            case "gmode2":
-                                                case "gmod2":
-                                                    // switch the gamemode to adventure
-                                                    player.setGameMode(ADVENTURE);
-                                                    break;
-                                                    case "gmsp":
-                                                        case "gm3":
-                                                            case "gmspectator":
-                                                                case "gspectator":
-                                                                    case "gmodespectator":
-                                                                        case "gamemodespectator":
-                                                                            case "gamemode3":
-                                                                                case "gmode3":
-                                                                                    case "gmod3":
-                                                                                        // switch the gamemode to spectator
-                                                                                        player.setGameMode(SPECTATOR);
-                                                                                        break;
+                case "gm2":
+                case "gmadventure":
+                case "gadventure":
+                case "gmodeadventure":
+                case "gamemodeadventure":
+                case "gamemode2":
+                case "gmode2":
+                case "gmod2":
+                    // switch the gamemode to adventure
+                    player.setGameMode(ADVENTURE);
+                    break;
+                case "gmsp":
+                case "gm3":
+                case "gmspectator":
+                case "gspectator":
+                case "gmodespectator":
+                case "gamemodespectator":
+                case "gamemode3":
+                case "gmode3":
+                case "gmod3":
+                    // switch the gamemode to spectator
+                    player.setGameMode(SPECTATOR);
+                    break;
             }
         } else {
             commandSender.sendMessage("§4§lYou do not have permission to use this command");
